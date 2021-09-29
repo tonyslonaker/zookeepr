13df57a7ef9cee31c75c7c65c5e097c26089af0b
Tony Slonaker <tony.slonaker@gmail.com>
2021-09-29 01:41:57 -0600

    update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const { animals } = require('./data/animals');
+const { animals } = require('./zookeepr/data/animals');
 const express = require('express');
 const app = express();
 
-app.get('/api/animals', (req, res) => {
-    res.send('Hello!');
+app.get('/zookeepr/api/animals', (req, res) => {
+    let results = animals;
+    console.log(req.query)
+    res.json('results');
 });
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
