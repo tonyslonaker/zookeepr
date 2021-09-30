cce4e6ba28eb9f7d7d32262783c6596d110b599a
Tony Slonaker <tony.slonaker@gmail.com>
2021-09-30 13:50:48 -0600

    update code

--- server.js
+++ server.js
@@ -36,6 +36,14 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    console.log(body);
+    // our function's main code will go here!
+
+    // return finished code to post route for response
+    return body;
+}
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -55,7 +63,10 @@ app.get('/api/animals/:id', (req, res) => {
 
 app.post('/api/animals', (req, res) => {
     //req.body is where our incoming content will be
-    console.log(req.body);
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    //console.log(req.body);
     res.json(req.body);
 });
 
