7c562c09a77861e8f45bd82e26fa8f1d360d8d74
Tony Slonaker <tony.slonaker@gmail.com>
2021-09-30 13:30:34 -0600

    update app.use

--- server.js
+++ server.js
@@ -53,6 +53,17 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+app.post('/api/animals', (req, res) => {
+    //req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
+
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data
+app.use(express.json());
