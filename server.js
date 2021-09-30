2e218883a06a859e003c68a051c3c5383c23a159
Tony Slonaker <tony.slonaker@gmail.com>
2021-09-29 20:35:21 -0600

    Add Heroku

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
-const { animals } = require('./zookeepr/data/animals');
+const { animals } = require('../zookeepr/zookeepr/data/animals');
 const express = require('express');
 const app = express();
 
-app.get('/zookeepr/api/animals', (req, res) => {
+function filterByQuery(query, animalsArray) {
+    let filteredResults = animalsArray;
+    if (query.diet) {
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+      filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+  }
+
+  app.get('/api/animals', (req, res) => {
     let results = animals;
-    console.log(req.query)
-    res.json('results');
-});
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+  });
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
 });
